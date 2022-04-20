740f2f6108e35eb0ac22a19cba81dede394eaf2b
Angela <angelamchoi@gmail.com>
2022-04-19 22:41:02 -0400

    added context

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Letter = ({ letterPos, attemptVal }) => {
+  const letter = board[attemptVal][letterPos]; // access letter individually
   return <div className="letter">Letter</div>;
 };
 
