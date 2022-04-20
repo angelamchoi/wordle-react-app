d189aa54f62eaaa1ed2137749e53bee033f9bc6e
Angela <angelamchoi@gmail.com>
2022-04-19 22:47:34 -0400

    fixed app context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
+import {createContext, useState} from "react";
 import { boardDefault } from "./Words";
+
 
 export const AppContext = createContext();
 
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../App";
 
 const Letter = ({ letterPos, attemptVal }) => {
+  const { board } = useContext(AppContext);
   const letter = board[attemptVal][letterPos]; // access letter individually
   return <div className="letter">Letter</div>;
 };
